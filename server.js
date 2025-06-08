4178f29d13d3f2870e9818bcc498bec207f4a3fb
thanhdang435232 <dangthanh010319@gmail.com>
2025-06-08 10:52:59 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+// API nhận dữ liệu cảm biến
+app.post("/api/data", (req, res) => {
+  console.log("Dữ liệu nhận được:", req.body);
+  res.status(200).send("Data received");
+});
+
+// Giao diện thử nghiệm
+app.get("/", (req, res) => {
+  res.send("Server đang chạy!");
+});
+
+// Lắng nghe cổng
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App đang chạy trên port ${PORT}`);
+});
